aaee339504a322f9d27b454489bf207b6c455357
jdh4 <52128661+jdh4@users.noreply.github.com>
2024-05-23 10:38:47 -0400

    Update efficiency.py

--- efficiency.py
+++ efficiency.py
@@ -3,28 +3,42 @@ import gzip
 import base64
 import pandas as pd
 
-def get_stats_dict(x):
-  if not x or pd.isna(x) or x == "JS1:Short" or x == "JS1:None":
-    return {}
-  else:
-    return json.loads(gzip.decompress(base64.b64decode(x[4:])))
 
-def cpu_efficiency(d, elapsedraw, jobid, cluster, single=False, precision=1):
-  total = 0
-  total_used = 0
-  for node in d['nodes']:
-    try:
-      used  = d['nodes'][node]['total_time']
-      cores = d['nodes'][node]['cpus']
-    except:
-      print("total_time not found for {jobid} on {cluster}")
-      return (0, 1)  # dummy values that avoid division by zero later
-    else:
-      alloc = elapsedraw * cores  # equal to cputimeraw
-      total += alloc
-      total_used += used
-  if total_used > total: print("E: CPU efficiency:", jobid, cluster, total_used, total, flush=True)
-  return round(100 * total_used / total, precision) if single else (total_used, total)
+def get_stats_dict(ss64):
+    """Convert the base64-encoded summary statistics to JSON."""
+    if (not ss64) or pd.isna(ss64) or ss64 == "JS1:Short" or ss64 == "JS1:None":
+        return {}
+    return json.loads(gzip.decompress(base64.b64decode(ss64[4:])))
+
+
+def cpu_efficiency(ss, elapsedraw, jobid, cluster, single=False, precision=1, verbose=True):
+    """Return a (CPU time used, CPU time allocated, error code)-tuple for a given job.
+       If single=True then return a (CPU time used / CPU time allocated, error code)-tuple.
+       The error code is needed since the summary statistics (ss) may not contain the
+       expected keys."""
+    total = 0
+    total_used = 0
+    for node in ss['nodes']:
+        try:
+            used  = ss['nodes'][node]['total_time']
+            cores = ss['nodes'][node]['cpus']
+        except:
+            if verbose:
+                msg = "Warning: JSON probably missing keys in cpu_efficiency:"
+                print(msg, jobid, cluster, flush=True)
+            error_code = 1
+            return (-1, -1, error_code)
+        else:
+            alloc = elapsedraw * cores  # equal to cputimeraw
+            total += alloc
+            total_used += used
+    if total_used > total and verbose:
+        msg = "Warning: total_used > total in cpu_efficiency:"
+        print(msg, jobid, cluster, total_used, total, flush=True)
+    error_code = 0
+    if single:
+        return (round(100 * total_used / total, precision), error_code)
+    return  (total_used, total, error_code)
 
 def gpu_efficiency(d, elapsedraw, jobid, cluster, single=False, precision=1):
   total = 0
